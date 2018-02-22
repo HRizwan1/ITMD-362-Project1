ae8eb75012bcd2ef0230676d1848c9ef89c01e42
dkolov84 <31516121+dkolov84@users.noreply.github.com>
2018-02-22 00:01:40 -0600

    adding some jscript to get input and display alert

--- js/site.js
+++ js/site.js
@@ -3,3 +3,16 @@ jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
 });
+document.getElementById("submit").addEventListener("click", confirm);
+function confirm(event){
+  for(i = 0; i < document.getElementsByTagName("input").length; i++)
+  {
+    if(document.getElementsByTagName("input").item(i).value === "" ||
+        (document.getElementsByTagName("input").item(i).type === "radio" && document.getElementsByTagName("input").item(i).checked===false))
+        {
+          return false;
+        }
+  }
+  alert("You have successfully subscribed to our Newsletter!");
+  return true;
+}
